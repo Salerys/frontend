87cd49083d1a34f41f583baad9779ddf37051951
unknown <salerysofficial@gmail.com>
2024-11-12 22:41:24 +0100

    added login and logout functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function RegisterAndLogout() {
   localStorage.clear();
   return <Register />;
+}
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
 }
 
 function App() {
@@ -13,6 +19,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
     </BrowserRouter>
     </>
